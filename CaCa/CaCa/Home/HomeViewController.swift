59cbd454fed241bbaf2e7519fe44f68444910377
안소정 <80646857+ahneve@users.noreply.github.com>
2022-11-02 14:50:42 +0900

    [Feat] 첫 화면 StoryBoard to HomeViewController

---
+++ CaCa/CaCa/Home/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  CaCa
+//
+//  Created by 소정의 Mac on 2022/11/02.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
